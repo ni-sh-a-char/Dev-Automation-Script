b40f5f619cd5932f704095e5d9bfffa35ee45511
dsfksh <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2023-05-30 13:57:05 +0530

    correcting code

--- dash.py
+++ dash.py
@@ -16,7 +16,23 @@ import seedir as sd
 from contextlib import contextmanager, redirect_stdout
 from io import StringIO
 from time import sleep
-import subprocess 
+import subprocess
+import openai
+
+openai.api_key = ""
+
+def chat_with_chatgpt(prompt, model="gpt-3.5-turbo"):
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = response.choices[0].text.strip()
+    return message
 
 def main():
 
@@ -26,7 +42,7 @@ def main():
 
     st.title("Dev-Automation-Script(dash)")
 
-    menu = ["Intro","Directory Visualizer","Clone from GitHub", "Generate Patch", "Send-Mail", "Docker"]
+    menu = ["Intro","Directory Visualizer", "GitHub", "Generate Patch", "Send-Mail", "Docker", "E.T.E.R.GPT"]
     choice = st.sidebar.selectbox("Select Activity", menu)
     if choice == "Intro":
         st.subheader("Automate Development")
@@ -63,24 +79,134 @@ def main():
             with st_capture(output.code):
                 print(sd.seedir(dirname, style='emoji'))
 
-    elif choice == "Clone from GitHub":
-       st.subheader("Clone From Github")
-       root = tk.Tk()
-       root.withdraw()
+    elif choice == "GitHub":
+        st.subheader == "Work with Github"
+        menu = ["Clone From GitHub","Pull from GitHub", "Push to GitHub"]
+        choice = st.sidebar.selectbox("Select Activity", menu)
+        if choice == "Clone From GitHub":
+            st.subheader("Clone From GitHub")
+            root = tk.Tk()
+            root.withdraw()
 
         # Make folder picker dialog appear on top of other windows
-       root.wm_attributes('-topmost', 1)
+            root.wm_attributes('-topmost', 1)
+
         # Folder picker button
-       clone_url = st.text_input('Github URL')
-       st.title('Choose Location')
-       st.write('Please select your destination to clone:')
-       clicked = st.button("Start cloning")
-       if clicked:
-            dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
-            os.chdir(dirname)
-            process = subprocess.Popen (['git', 'clone', clone_url],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            result = process.communicate( )
-            st.write(result)
+            st.subheader("Please select a destination to Clone")
+            destination = st.button('Select Destination')
+            if destination:
+                dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
+                @contextmanager
+                def st_capture(output_func):
+                    with StringIO() as stdout, redirect_stdout(stdout):
+                        old_write = stdout.write
+
+                    def new_write(string):
+                        ret = old_write(string)
+                        output_func(stdout.getvalue())
+                        return ret
+                    
+                    stdout.write = new_write
+                    yield
+
+                output = st.empty()
+                with st_capture(output.code):
+                    print(sd.seedir(dirname, style='emoji'))
+                    
+                clone_url = st.text_input('Github URL')
+                clone = st.button("Start cloning")
+                if clone:
+                    dirname = st.text_input('Selected folder:', filedialog.askdirectory(master=root))
+                    os.chdir(dirname)
+                    process = subprocess.Popen (['git', 'clone', clone_url],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result = process.communicate( )
+                    st.write(result)
+        
+        elif choice == "Pull from GitHub":
+            st.subheader("Pull from GitHub")
+        # Set up tkinter
+            root = tk.Tk()
+            root.withdraw()
+
+        # Make folder picker dialog appear on top of other windows
+            root.wm_attributes('-topmost', 1)
+
+        # Folder picker button
+            clicked = st.button('View')
+            if clicked:
+                dirname = os.getcwd()
+                @contextmanager
+                def st_capture(output_func):
+                    with StringIO() as stdout, redirect_stdout(stdout):
+                        old_write = stdout.write
+
+                    def new_write(string):
+                        ret = old_write(string)
+                        output_func(stdout.getvalue())
+                        return ret
+                    
+                    stdout.write = new_write
+                    yield
+
+
+                output = st.empty()
+                with st_capture(output.code):
+                    print(sd.seedir(dirname, style='emoji'))
+                clicked = st.button('Pull')
+                if clicked:
+                    process = subprocess.Popen (['git', 'pull'],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result = process.communicate( )
+                    st.write(result)
+
+        elif choice == "Push to GitHub":
+            st.subheader("Push to GitHub")
+        # Set up tkinter
+            root = tk.Tk()
+            root.withdraw()
+
+        # Make folder picker dialog appear on top of other windows
+            root.wm_attributes('-topmost', 1)
+
+        # Folder picker button
+            clicked = st.button('View')
+            if clicked:
+                dirname = os.getcwd()
+                @contextmanager
+                def st_capture(output_func):
+                    with StringIO() as stdout, redirect_stdout(stdout):
+                        old_write = stdout.write
+
+                    def new_write(string):
+                        ret = old_write(string)
+                        output_func(stdout.getvalue())
+                        return ret
+                    
+                    stdout.write = new_write
+                    yield
+
+
+                output = st.empty()
+                with st_capture(output.code):
+                    print(sd.seedir(dirname, style='emoji'))
+                commit_message = st.text_input("Enter commit message")
+                clicked = st.button('Push')
+                if clicked:
+                    process1 = subprocess.Popen (['git', 'init'],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result1 = process1.communicate( )
+                    st.write(result1)
+                    subprocess.wait()
+                    process2 = subprocess.Popen (['git', 'add.'],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result2 = process2.communicate( )
+                    st.write(result2)
+                    subprocess.wait()
+                    process3 = subprocess.Popen (['git', 'commit', '-m', commit_message],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result3 = process3.communicate( )
+                    st.write(result3)
+                    subprocess.wait()
+                    process4 = subprocess.Popen (['git', 'push'],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    result4 = process4.communicate( )
+                    st.write(result4)
+
         
 
     elif choice == "Send-Mail":
@@ -201,6 +327,15 @@ def main():
                 process = subprocess.Popen (['docker','build', '.'],shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 result = process.communicate( )
                 st.write(result)
+
+    elif choice == "E.T.E.R.GPT":
+        st.title("E.T.E.R.GPT")
+        st.subheader("Create Code With GPT")
+        user_prompt = st.text_area("Enter Your Query")
+        click = st.button("Generate Response")
+        if click:
+            chatbot_response = chat_with_chatgpt(user_prompt)
+            st.write(chatbot_response)    
         
 
 if __name__ == '__main__':
